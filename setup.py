557fea42404700db959bcb631e9223db7f12736c
Midhun <meldose@gmail.com>
2024-10-27 21:53:04 +0100

    added comments in setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from distutils.core import setup # import setup module from distutils
+from catkin_pkg.python_setup import generate_distutils_setup # import generate_distutils_setup from catkin
 
-setup_args = generate_distutils_setup(
-    packages=['interfaces'],
-    package_dir={'': 'test'},
+setup_args = generate_distutils_setup( # setup arguments
+    packages=['interfaces'], # set packages as list having package names as strings called by interfaces
+    package_dir={'': 'test'}, # set package directory as '': 'test'
 )
 
-setup(**setup_args)
+setup(**setup_args) # call setup function
